b8e6914be9e1d85d8d890ad844fcb511362f7f38
Peter Kepes <kepesp@gmail.com>
2022-12-21 18:45:09 +0100

    cosmetic changes in intaimage route

--- server/routes/instaimage.js
+++ server/routes/instaimage.js
@@ -1,21 +1,18 @@
-"use strict";
+'use strict';
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-// Not working
+// TODO Not working
 // Issue: https://github.com/strapi/strapi/issues/15050
-//module.exports = createCoreRouter('plugin::instagram.instaimage');
+// const { createCoreRouter } = require('@strapi/strapi').factories;
+// module.exports = createCoreRouter('plugin::instagram.instaimage');
 
 module.exports = {
-    type: 'content-api',
-    routes: [
-      {
-        method: 'GET',
-        path: '/images',
-        handler: 'instaimage.find', 
-        config: {
-          //auth: false,
-        },
-      },
-    ],
-  }
+  type: 'content-api',
+  routes: [
+    {
+      method: 'GET',
+      path: '/images',
+      handler: 'instaimage.find',
+      config: {},
+    },
+  ],
+};
